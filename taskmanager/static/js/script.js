a4dec269af5fd3a2b40e59c49fc20ef55ca658bc
Ruth De Swart <ruthdeswart@outlook.ie>
2024-10-14 09:25:50 +0000

    Build CRUD functionality for Tasks

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,5 +1,16 @@
-// sidenav initialization
-document.addEventListener('DOMContentLoaded', function() {
-    let sidenav = document.querySelectorAll('.sidenav');
-    M.Sidenav.init(sidenav);
+document.addEventListener('DOMContentLoaded', function () {
+  // sidenav initialization
+  let sidenav = document.querySelectorAll('.sidenav');
+  M.Sidenav.init(sidenav);
+
+  // datepicker initialization
+  let datepicker = document.querySelectorAll('.datepicker');
+  M.Datepicker.init(datepicker, {
+      format: "dd mmm, yyyy",
+      i18n: { done: "Select"}
   });
+
+  // select initialization
+  let selects = document.querySelectorAll('select');
+  M.FormSelect.init(selects);
+});
